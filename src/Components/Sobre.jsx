585976e277bb066c12e9f02abb5fa5faab48d263
Ray Martins <rhaymarthins@gmail.com>
2023-12-03 23:23:40 -0300

    Alteração componente Sobre

--- src/Components/Sobre.jsx
+++ src/Components/Sobre.jsx
@@ -1,7 +1,16 @@
+import React from "react";
+import { Main, Imagem, Descricao } from "../styles/SobreStyle";
+import ImagemSobre from "../assets/about.png";
+
 export default function Sobre() {
     return (
         <>
-            <h1>Sobre</h1>
+            <Main>
+                <Imagem src={ImagemSobre} alt="facebook" />
+                <Descricao>
+                    Sou estudante de Engenharia de Computação na Universidade Federal do Ceará. Possuo um breve conhecimento em data science e atualmente estudando tecnologias para desenvolvimento web, tendo recentemente um foco maior com as tecnologias React, Vue.js e Laravel.
+                </Descricao>
+            </Main>
         </>
     )
 }
---
+++ src/styles/SobreStyle.jsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const Main = styled.main`
+    height: 80vh;
+    background-color: #0B2C4D;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    color: white;
+`
+
+export const Imagem = styled.img`
+    // border:solid;
+    height: 50vh;
+    width: 40vw;
+`
+
+export const Descricao = styled.section`
+    // border:solid;
+    display: flex;
+    align-items: center;
+    height: 50vh;
+    width: 40vw;
+    font-size: 28px
+`
