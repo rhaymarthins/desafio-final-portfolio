330b2067c42f87b4c6d6578de7a94a03dadd8f30
Ray Martins <rhaymarthins@gmail.com>
2023-12-04 23:51:55 -0300

    Atualizando o componente Footer

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Section } from "../styles/FooterStyle";
+
+export default function Footer() {
+    return (
+        <>
+            <Section>
+                <p>Todos os direitos reservados © 2023</p>
+            </Section>
+        </>
+    )
+}
---
+++ src/styles/FooterStyle.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Section = styled.section`
+    background-color: #0D0D0D;
+    height: 10vh;
+    color: #F4F4F2;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
